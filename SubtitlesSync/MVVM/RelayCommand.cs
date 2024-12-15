efd3dc561f23efcb5784462e5fd8352cfb08e876
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2024-12-15 22:50:36 +0100

    RelayCommand - Changing internal to public

--- SubtitlesSync/MVVM/RelayCommand.cs
+++ SubtitlesSync/MVVM/RelayCommand.cs
@@ -2,7 +2,7 @@
 
 namespace SubtitlesSync.MVVM
 {
-    internal class RelayCommand : ICommand
+    public class RelayCommand : ICommand
     {
         private Action<object> execute;
         private Func<object, bool> canExecute;
